61c02a245848298bb324b03c60051cab2be99749
Keme Kenneth <keme.kenneth@gmail.com>
2023-06-15 19:19:18 +0100

    html

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ httpServer.listen(PORT, () => console.log('Server started at', PORT))
 app.get('/', (req, res) => {
     console.log('https will fail \'cos ssl isn\'t installed yet. just empty object');
     const html = `
-        <h1 style="font-size:5rem; display:flex; justify-content:center; align-items:center; block-size:100vh; -webkit-text-fill-color:transparent; -webkit-background-clip:text; background-clip: text; background-image: linear-gradient(90deg, #00BCD4, #FF9800);">CI/CD - express app - github to ec2</h1>
+        <h1 style="font-size:4rem; display:flex; justify-content:center; align-items:center; block-size:100vh; -webkit-text-fill-color:transparent; -webkit-background-clip:text; background-clip: text; background-image: linear-gradient(90deg, #00BCD4, #FF9800);">CI/CD - express app - github action to ec2</h1>
     `
 
     res.send(html).status(200)
